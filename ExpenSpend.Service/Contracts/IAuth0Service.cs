c173bf5c1a332f6139feebe06bfefe3893bdb1ba
Rahul Kumar <r1303yadav@gmail.com>
2024-04-05 18:09:49 +0530

    Implement Auth0 login

---
+++ ExpenSpend.Service/Contracts/IAuth0Service.cs
@@ -0,0 +1,14 @@
+﻿using ExpenSpend.Domain.DTOs.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenSpend.Service.Contracts
+{
+    public interface IAuth0Service
+    {
+        Task<GetAuth0UserDto> GetUserInfo(string accessToken);
+    }
+}
