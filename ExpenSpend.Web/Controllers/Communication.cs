96e48673fb62b1b51d696e97b39a21f637b28d4e
RAHUL-COD3 <rahul@promactinfo.com>
2024-07-19 15:29:18 +0530

    Add communications API

---
+++ ExpenSpend.Web/Controllers/Communication.cs
@@ -0,0 +1,20 @@
+﻿using ExpenSpend.Domain.DTOs.Communication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ExpenSpend.Web.Controllers
+{
+    [ApiController]
+    [Route("api/communication")]
+    //[Authorize]
+    public class Communication : ControllerBase
+    {
+        [HttpPost("message")]
+        public async Task<IActionResult> MessageAsync(CommunicationDto inputs)
+        {
+            var result =  "Response is: "+inputs.input;
+            return new OkObjectResult(result);
+        }
+    }
+}
